a74c6987a2be2e4f5334c3b56943c03284bfa42c
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-01-10 19:54:57 -0500

    Add SubmissionMessage class with validation logic

---
+++ code-executor/Tsa.Submissions.Coding.CodeExecutor.Worker/Messages/SubmissionMessage.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Tsa.Submissions.Coding.CodeExecutor.Worker.Messages;
+
+public class SubmissionMessage
+{
+    [JsonPropertyName("submissionId")]
+    public Guid SubmissionId { get; set; }
+
+    [JsonPropertyName("submittedAt")]
+    public DateTimeOffset SubmittedAt { get; set; }
+
+    public void EnsureMessageIsValid()
+    {
+        if (SubmissionId == Guid.Empty) throw new InvalidOperationException("SubmissionId cannot be default");
+
+        if (SubmittedAt == DateTimeOffset.MinValue) throw new InvalidOperationException("SubmittedAt cannot be default");
+    }
+}
