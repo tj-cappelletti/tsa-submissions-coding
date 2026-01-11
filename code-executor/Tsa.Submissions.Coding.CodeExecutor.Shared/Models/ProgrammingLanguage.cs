c1a2898be419980235fc6a213d1084743a0350ae
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-01-10 19:54:24 -0500

    Add initial models for ProgrammingLanguage, SubmissionModel, TeamModel, TestSetResultModel, and UserModel

---
+++ code-executor/Tsa.Submissions.Coding.CodeExecutor.Shared/Models/ProgrammingLanguage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tsa.Submissions.Coding.CodeExecutor.Shared.Models
+{
+    public class ProgrammingLanguage
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public string Version { get; set; } = string.Empty;
+    }
+}
---
+++ code-executor/Tsa.Submissions.Coding.CodeExecutor.Shared/Models/TestSetResultModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Tsa.Submissions.Coding.CodeExecutor.Shared.Models;
+
+public class TestSetResultModel
+{
+    public bool Passed { get; set; }
+
+    public TimeSpan? RunDuration { get; set; }
+
+    public string? TestSetId { get; set; }
+}
