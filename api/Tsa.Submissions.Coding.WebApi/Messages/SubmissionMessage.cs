8824afe721acd7b2560288a1e07700b1235f607c
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-01-14 09:04:44 -0500

    Created SubmissionMessage class
    Used to send messages on RabbitMQ

---
+++ api/Tsa.Submissions.Coding.WebApi/Messages/SubmissionMessage.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Text.Json.Serialization;
+
+namespace Tsa.Submissions.Coding.WebApi.Messages;
+
+public class SubmissionMessage
+{
+    [JsonPropertyName("submissionId")]
+    public string? SubmissionId { get; set; }
+
+    [JsonPropertyName("submittedAt")]
+    public DateTimeOffset SubmittedAt { get; set; }
+
+    public void EnsureMessageIsValid()
+    {
+        if (string.IsNullOrWhiteSpace(SubmissionId)) throw new InvalidOperationException("SubmissionId cannot be default");
+
+        if (SubmittedAt == DateTimeOffset.MinValue) throw new InvalidOperationException("SubmittedAt cannot be default");
+    }
+}
