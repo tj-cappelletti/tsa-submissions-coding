0a54eb2775352937524bbf88c273c09fecfee911
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-01-17 11:42:18 -0500

    Added configuration classes for API

---
+++ code-executor/Tsa.Submissions.Coding.CodeExecutor.Worker/Configuration/ApiAuthConfig.cs
@@ -0,0 +1,8 @@
+namespace Tsa.Submissions.Coding.CodeExecutor.Worker.Configuration;
+
+public class ApiAuthConfig
+{
+    public string Password { get; set; } = string.Empty;
+
+    public string Username { get; set; } = string.Empty;
+}
---
+++ code-executor/Tsa.Submissions.Coding.CodeExecutor.Worker/Configuration/SubmissionsApiConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace Tsa.Submissions.Coding.CodeExecutor.Worker.Configuration;
+
+public class SubmissionsApiConfig
+{
+    public const string SectionName = "SubmissionsApi";
+
+    public ApiAuthConfig Authentication { get; set; } = new();
+
+    public string BaseUrl { get; set; } = string.Empty;
+}
