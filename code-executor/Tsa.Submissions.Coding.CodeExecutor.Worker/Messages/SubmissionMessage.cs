5cb811de009a1d215769cd149913e9f2cb2464cd
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-01-17 11:42:34 -0500

    Fixed ID for Submissions

--- code-executor/Tsa.Submissions.Coding.CodeExecutor.Worker/Messages/SubmissionMessage.cs
+++ code-executor/Tsa.Submissions.Coding.CodeExecutor.Worker/Messages/SubmissionMessage.cs
@@ -5,14 +5,14 @@ namespace Tsa.Submissions.Coding.CodeExecutor.Worker.Messages;
 public class SubmissionMessage
 {
     [JsonPropertyName("submissionId")]
-    public Guid SubmissionId { get; set; }
+    public string? SubmissionId { get; set; }
 
     [JsonPropertyName("submittedAt")]
     public DateTimeOffset SubmittedAt { get; set; }
 
     public void EnsureMessageIsValid()
     {
-        if (SubmissionId == Guid.Empty) throw new InvalidOperationException("SubmissionId cannot be default");
+        if (string.IsNullOrWhiteSpace(SubmissionId)) throw new InvalidOperationException("SubmissionId cannot be default");
 
         if (SubmittedAt == DateTimeOffset.MinValue) throw new InvalidOperationException("SubmittedAt cannot be default");
     }
