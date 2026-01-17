b2218e1cd507918945fd1fe714ad3a75c76b0a09
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-01-17 11:40:29 -0500

    Created models to handle logins to API

---
+++ code-executor/Tsa.Submissions.Coding.CodeExecutor.Shared/Models/AuthenticationModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Tsa.Submissions.Coding.CodeExecutor.Shared.Models;
+
+public class AuthenticationModel
+{
+    public string? Password { get; set; }
+
+    public string? UserName { get; set; }
+}
---
+++ code-executor/Tsa.Submissions.Coding.CodeExecutor.Shared/Models/LoginResponseModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Tsa.Submissions.Coding.CodeExecutor.Shared.Models;
+
+public class LoginResponseModel
+{
+    [JsonPropertyName("expiration")]
+    public DateTimeOffset Expiration { get; set; }
+
+    [JsonPropertyName("token")]
+    public string Token { get; set; } = string.Empty;
+}
