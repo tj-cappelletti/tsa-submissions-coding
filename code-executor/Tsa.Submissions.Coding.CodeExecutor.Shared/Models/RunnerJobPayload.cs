a537ce8c82e5b577db09631d0ce6c851a1514e92
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-01-18 11:51:58 -0500

    Add RunnerJobPayload and RunnerJobResult models for code execution

---
+++ code-executor/Tsa.Submissions.Coding.CodeExecutor.Shared/Models/RunnerJobPayload.cs
@@ -0,0 +1,12 @@
+﻿namespace Tsa.Submissions.Coding.CodeExecutor.Shared.Models;
+
+public record RunnerJobPayload
+{
+    public required ProgrammingLanguage Language { get; init; }
+
+    public required string ProblemId { get; init; }
+
+    public required string Solution { get; init; }
+
+    public required string SubmissionId { get; init; }
+}
---
+++ code-executor/Tsa.Submissions.Coding.CodeExecutor.Shared/Models/RunnerJobResult.cs
@@ -0,0 +1,24 @@
+﻿namespace Tsa.Submissions.Coding.CodeExecutor.Shared.Models;
+
+public record RunnerJobResult
+{
+    /// <summary>
+    ///     Indicates whether the code compiled successfully.
+    /// </summary>
+    public bool CodeCompiled { get; init; }
+
+    /// <summary>
+    ///     Indicates whether the job failed.
+    /// </summary>
+    public bool IsFailure => !IsSuccessful;
+
+    /// <summary>
+    ///     Indicates whether the job completed successfully.
+    /// </summary>
+    public bool IsSuccessful { get; init; }
+
+    /// <summary>
+    ///     The results of each test set executed as part of the job.
+    /// </summary>
+    public IList<TestSetResultModel> TestSetResults { get; init; } = new List<TestSetResultModel>();
+}
