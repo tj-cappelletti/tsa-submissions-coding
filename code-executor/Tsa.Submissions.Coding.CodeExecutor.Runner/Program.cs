7f4e17337872902d69177a75e6a34d7d65940953
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-01-18 11:51:04 -0500

    Created placeholder/empty shell for tool execution
    This is to test image building and job scheduling

---
+++ code-executor/Tsa.Submissions.Coding.CodeExecutor.Runner/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tsa.Submissions.Coding.CodeExecutor.Shared.Models;
+
+namespace Tsa.Submissions.Coding.CodeExecutor.Runner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Runner starting up...");
+
+            var submission = new SubmissionModel
+            {
+                Id = "test"
+            };
+            Console.WriteLine($"Mocking running process - Submission ID: {submission.Id}...");
+            var sleepTimeSpan = TimeSpan.FromMinutes(1);
+            Thread.Sleep(sleepTimeSpan);
+
+            Console.WriteLine("Shutting down...");
+        }
+    }
+}
