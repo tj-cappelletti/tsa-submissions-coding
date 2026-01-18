28a1186ab085d9427bdb78e220d87e7fb2f95450
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-01-18 11:53:06 -0500

    Created configuration classes

---
+++ code-executor/Tsa.Submissions.Coding.CodeExecutor.Worker/Configuration/RunnerImageRegistry.cs
@@ -0,0 +1,37 @@
+﻿namespace Tsa.Submissions.Coding.CodeExecutor.Worker.Configuration;
+
+public class RunnerImageRegistry
+{
+    public const string SectionName = "RunnerImageRegistry";
+
+    public string? ImageName { get; set; }
+
+    public string? ImageVersion { get; set; }
+
+    public LanguageTags? LanguageTags { get; set; }
+
+    public string? RegistryUrl { get; set; }
+}
+
+public class LanguageTags
+{
+    public string? C { get; set; }
+
+    public string? Cpp { get; set; }
+
+    public string? CSharp { get; set; }
+
+    public string? FSharp { get; set; }
+
+    public string? Go { get; set; }
+
+    public string? Java { get; set; }
+
+    public string? NodeJs { get; set; }
+
+    public string? Python { get; set; }
+
+    public string? Ruby { get; set; }
+
+    public string? VisualBasic { get; set; }
+}
