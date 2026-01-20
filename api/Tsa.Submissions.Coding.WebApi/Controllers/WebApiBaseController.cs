adb21c4d53a83c2da225bf82eb73a33b9e56e1cd
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-01-20 10:33:34 -0500

    Added WebApiBaseController with model validation method

---
+++ api/Tsa.Submissions.Coding.WebApi/Controllers/WebApiBaseController.cs
@@ -0,0 +1,36 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using FluentValidation;
+using Microsoft.AspNetCore.Mvc;
+using Tsa.Submissions.Coding.WebApi.Validators;
+
+namespace Tsa.Submissions.Coding.WebApi.Controllers;
+
+public abstract class WebApiBaseController : ControllerBase
+{
+    /// <summary>
+    ///     A generic method to validate a model using a specified FluentValidation validator.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="model"></param>
+    /// <param name="validator"></param>
+    /// <returns>The <see cref="ValidatedResult" /> representing the outcome of the validation.</returns>
+    protected async Task<ValidatedResult> ValidateAsync<T>(T model, IValidator<T> validator)
+    {
+        var result = await validator.ValidateAsync(model);
+
+        if (result.IsValid)
+        {
+            return ValidatedResult.Success();
+        }
+
+        return ValidatedResult.Failure(BadRequest(new
+        {
+            errors = result.Errors.Select(selector: e => new
+            {
+                field = e.PropertyName,
+                message = e.ErrorMessage
+            })
+        }));
+    }
+}
