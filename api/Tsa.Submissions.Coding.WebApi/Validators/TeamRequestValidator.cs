bd7b117c117be719b656c7a399a340ef6c9c7a1b
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-01-20 09:05:11 -0500

    Renamed validator to match new TeamRequest contract

--- api/Tsa.Submissions.Coding.WebApi/Validators/TeamRequestValidator.cs
+++ api/Tsa.Submissions.Coding.WebApi/Validators/TeamRequestValidator.cs
@@ -1,40 +1,42 @@
 ﻿using System;
 using FluentValidation;
+using Tsa.Submissions.Coding.Contracts.Users;
 using Tsa.Submissions.Coding.WebApi.Entities;
-using Tsa.Submissions.Coding.WebApi.Models;
 
 namespace Tsa.Submissions.Coding.WebApi.Validators;
 
-public class TeamModelValidator : AbstractValidator<TeamModel>
+public class TeamRequestValidator : AbstractValidator<TeamRequest>
 {
     private const string ValidSchoolNumberRegEx = @"(?:1|2)[\d]{3}";
     private const string ValidTeamNumberRegEx = @"9[\d]{2}";
 
-    public TeamModelValidator()
+    public TeamRequestValidator()
     {
-        RuleFor(team => team.CompetitionLevel)
+        RuleFor(expression: team => team.CompetitionLevel)
             .NotEmpty()
             .IsEnumName(typeof(CompetitionLevel), false)
             .WithMessage("The competition level must be either 'MiddleSchool' or 'HighSchool'.");
 
-        RuleFor(team => team.SchoolNumber)
+        RuleFor(expression: team => team.SchoolNumber)
             .NotEmpty()
             .Matches(ValidSchoolNumberRegEx)
             .WithMessage("The School Number is required and must be 4 digits in length and start with a 1 for Middle School and 2 for High School")
-            .Must(schoolNumber => schoolNumber.StartsWith('1'))
-            .When(team => string.Equals(team.CompetitionLevel, CompetitionLevel.MiddleSchool.ToString(), StringComparison.CurrentCultureIgnoreCase), ApplyConditionTo.CurrentValidator)
+            .Must(predicate: schoolNumber => schoolNumber.StartsWith('1'))
+            .When(
+                predicate: team =>
+                    string.Equals(team.CompetitionLevel, CompetitionLevel.MiddleSchool.ToString(), StringComparison.CurrentCultureIgnoreCase),
+                ApplyConditionTo.CurrentValidator)
             .WithMessage("The School Number must start with a 1 when the competition level is Middle School")
-            .Must(schoolNumber => schoolNumber.StartsWith('2'))
-            .When(team => string.Equals(team.CompetitionLevel, CompetitionLevel.HighSchool.ToString(), StringComparison.CurrentCultureIgnoreCase), ApplyConditionTo.CurrentValidator)
+            .Must(predicate: schoolNumber => schoolNumber.StartsWith('2'))
+            .When(
+                predicate: team =>
+                    string.Equals(team.CompetitionLevel, CompetitionLevel.HighSchool.ToString(), StringComparison.CurrentCultureIgnoreCase),
+                ApplyConditionTo.CurrentValidator)
             .WithMessage("The School Number must start with a 2 when the competition level is High School");
 
-        RuleFor(team => team.TeamNumber)
+        RuleFor(expression: team => team.TeamNumber)
             .NotEmpty()
             .Matches(ValidTeamNumberRegEx)
             .WithMessage("The Team Number is required and must 3 digits that starts with a 9");
-
-        //RuleFor(team => team.Participants)
-        //    .SetValidator(new ParticipantsPropertyValidator())
-        //    .WithMessage("The participant(s) are not valid");
     }
 }
