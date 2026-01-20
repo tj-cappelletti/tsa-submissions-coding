4943a9f0ebdb6e8a1c473c50eb231e8a13730014
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-01-20 13:48:10 -0500

    Applied code formatting to WebApiBaseController

--- api/Tsa.Submissions.Coding.WebApi/Controllers/WebApiBaseController.cs
+++ api/Tsa.Submissions.Coding.WebApi/Controllers/WebApiBaseController.cs
@@ -14,8 +14,8 @@ public abstract class WebApiBaseController : ControllerBase
     /// </summary>
     /// <typeparam name="T">The type of the model being validated.</typeparam>
     /// <param name="model">The instance of the model to validate.</param>
-    /// <param name="validator">The <see cref="IValidator{T}"/> instance used for validation.</param>
-    /// <param name="cancellationToken">A <see cref="CancellationToken"/> to observe while waiting for the task to complete.</param>
+    /// <param name="validator">The <see cref="IValidator{T}" /> instance used for validation.</param>
+    /// <param name="cancellationToken">A <see cref="CancellationToken" /> to observe while waiting for the task to complete.</param>
     /// <returns>The <see cref="ValidatedResult" /> representing the outcome of the validation.</returns>
     protected async Task<ValidatedResult> ValidateAsync<T>(T model, IValidator<T> validator, CancellationToken cancellationToken)
     {
@@ -28,7 +28,7 @@ public abstract class WebApiBaseController : ControllerBase
 
         return ValidatedResult.Failure(BadRequest(new
         {
-            errors = result.Errors.Select(selector: e => new
+            errors = result.Errors.Select(e => new
             {
                 field = e.PropertyName,
                 message = e.ErrorMessage
