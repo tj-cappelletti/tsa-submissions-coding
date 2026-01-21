ab503826426ed5768b6466138211c66b87ee16d9
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-01-21 05:49:16 -0500

    Added error response methods to WebApiBaseController
    This improves and standardize API error handling

--- api/Tsa.Submissions.Coding.WebApi/Controllers/WebApiBaseController.cs
+++ api/Tsa.Submissions.Coding.WebApi/Controllers/WebApiBaseController.cs
@@ -1,14 +1,61 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
+using Tsa.Submissions.Coding.Contracts;
+using Tsa.Submissions.Coding.WebApi.Models;
 using Tsa.Submissions.Coding.WebApi.Validators;
 
 namespace Tsa.Submissions.Coding.WebApi.Controllers;
 
 public abstract class WebApiBaseController : ControllerBase
 {
+    public static ApiErrorResponse ApiErrorEntityAlreadyExists(string entityName, string lookupKey)
+    {
+        var data = new Dictionary<string, string>
+        {
+            { "entityName", entityName },
+            { "lookupKey", lookupKey }
+        };
+
+        return new ApiErrorResponse(data, (int)ErrorCodes.EntityAlreadyExists, "The resource requested to create already exists.");
+    }
+
+    public static ApiErrorResponse ApiErrorEntityNotFound(string entityName, string lookupKey)
+    {
+        var data = new Dictionary<string, string>
+        {
+            { "entityName", entityName },
+            { "lookupKey", lookupKey }
+        };
+
+        return new ApiErrorResponse(data, (int)ErrorCodes.EntityNotFound, "The requested resource could not be found.");
+    }
+
+    public static ApiErrorResponse ApiErrorInvalidId()
+    {
+        return new ApiErrorResponse((int)ErrorCodes.InvalidId, "The ID provided is not valid.");
+    }
+
+    public static ApiErrorResponse ApiErrorUnauthorized()
+    {
+        return new ApiErrorResponse((int)ErrorCodes.Unauthorized, "Client is unauthorized");
+    }
+
+    public static ApiErrorResponse ApiErrorUnexpectedMissingResource()
+    {
+        return new ApiErrorResponse((int)ErrorCodes.UnexpectedMissingResource,
+            "A dependent resource could not be loaded while making this call.");
+    }
+
+    public static ApiErrorResponse ApiErrorUnexpectedNullValue()
+    {
+        return new ApiErrorResponse((int)ErrorCodes.UnexpectedNullValue,
+            "A required value was unexpectedly missing.");
+    }
+
     /// <summary>
     ///     A generic method to validate a model using a specified FluentValidation validator.
     /// </summary>
