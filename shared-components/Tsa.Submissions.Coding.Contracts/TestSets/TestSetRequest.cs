1c2076dbdd80ed24ae493efa1673745d8ab209e3
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-01-21 09:51:51 -0500

    Formatted code for TestSetRequest

--- shared-components/Tsa.Submissions.Coding.Contracts/TestSets/TestSetRequest.cs
+++ shared-components/Tsa.Submissions.Coding.Contracts/TestSets/TestSetRequest.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Tsa.Submissions.Coding.Contracts.TestSets;
+﻿namespace Tsa.Submissions.Coding.Contracts.TestSets;
 
 public record TestSetRequest(
     IList<TestSetValueRequest> Inputs,
     bool IsPublic,
     string Name,
     string ProblemId);
-    
