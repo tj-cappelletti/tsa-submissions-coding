959ce34282b11cfcb97af776487b9e79aa2112cd
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-01-22 07:07:09 -0500

    Removed old shared project
    This was replaced with the Contracts project

--- code-executor/Tsa.Submissions.Coding.CodeExecutor.Shared/Models/AuthenticationModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Tsa.Submissions.Coding.CodeExecutor.Shared.Models;
-
-public class AuthenticationModel
-{
-    public string? Password { get; set; }
-
-    public string? UserName { get; set; }
-}
