9ea28139a725eb16b7f3b922918408a713ad56a0
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-01-22 14:17:18 -0500

    Refactored SubmissionMessage to set JsonPropertyName for attribute serialization

--- shared-components/Tsa.Submissions.Coding.Contracts/Messages/SubmissionMessage.cs
+++ shared-components/Tsa.Submissions.Coding.Contracts/Messages/SubmissionMessage.cs
@@ -1,7 +1,26 @@
-﻿namespace Tsa.Submissions.Coding.Contracts.Messages;
+﻿using System.Text.Json.Serialization;
 
-public record SubmissionMessage(
-    string ProblemId,
-    string SubmissionId,
-    DateTimeOffset SubmittedOn,
-    string UserId);
+namespace Tsa.Submissions.Coding.Contracts.Messages;
+
+public record SubmissionMessage
+{
+    [JsonPropertyName("problemId")]
+    public string ProblemId { get; }
+
+    [JsonPropertyName("submissionId")]
+    public string SubmissionId { get; }
+
+    [JsonPropertyName("submittedOn")]
+    public DateTimeOffset SubmittedOn { get; }
+
+    [JsonPropertyName("userId")]
+    public string UserId { get; }
+
+    public SubmissionMessage(string problemId, string submissionId, DateTimeOffset submittedOn, string userId)
+    {
+        ProblemId = problemId;
+        SubmissionId = submissionId;
+        SubmittedOn = submittedOn;
+        UserId = userId;
+    }
+}
