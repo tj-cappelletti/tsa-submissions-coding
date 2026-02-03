9dd8266d5277f091e173c05ae0fc497be0b58041
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-02-02 19:12:06 -0500

    Refactor RunnerJobPayload to use properties for better serialization and maintainability

--- shared-components/Tsa.Submissions.Coding.Contracts/CodeExecutor/RunnerJobPayload.cs
+++ shared-components/Tsa.Submissions.Coding.Contracts/CodeExecutor/RunnerJobPayload.cs
@@ -1,11 +1,46 @@
-﻿using Tsa.Submissions.Coding.Contracts.TestCases;
+﻿using System.Text.Json.Serialization;
+using Tsa.Submissions.Coding.Contracts.TestCases;
 
 namespace Tsa.Submissions.Coding.Contracts.CodeExecutor;
 
-public record RunnerJobPayload(
-    string Language,
-    string LanguageVersion,
-    string ProblemId,
-    string Solution,
-    string SubmissionId,
-    List<TestCase> TestCases);
+public record RunnerJobPayload
+{
+    [JsonPropertyName("language")]
+    public string Language { get; init; }
+
+    [JsonPropertyName("languageFixture")]
+    public string LanguageFixture { get; init; }
+
+    [JsonPropertyName("languageVersion")]
+    public string LanguageVersion { get; init; }
+
+    [JsonPropertyName("problemId")]
+    public string ProblemId { get; init; }
+
+    [JsonPropertyName("solution")]
+    public string Solution { get; init; }
+
+    [JsonPropertyName("submissionId")]
+    public string SubmissionId { get; init; }
+
+    [JsonPropertyName("testCases")]
+    public List<TestCase> TestCases { get; init; }
+
+    public RunnerJobPayload(
+        string language,
+        string languageFixture,
+        string languageVersion,
+        string problemId,
+        string solution,
+        string submissionId,
+        List<TestCase> testCases)
+    {
+        Language = language;
+        LanguageFixture = languageFixture;
+        LanguageVersion = languageVersion;
+        ProblemId = problemId;
+        Solution = solution;
+        SubmissionId = submissionId;
+        TestCases = testCases;
+    }
+}
