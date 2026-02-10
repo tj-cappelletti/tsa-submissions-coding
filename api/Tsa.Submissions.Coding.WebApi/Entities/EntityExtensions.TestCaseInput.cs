cf0aea6a61e4f748ffd4e35ba23c908d1e75308f
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-02-10 08:03:10 -0500

    Add EntityExtensions for TestCase and TestCaseInput

---
+++ api/Tsa.Submissions.Coding.WebApi/Entities/EntityExtensions.TestCaseInput.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tsa.Submissions.Coding.Contracts.TestCases;
+
+namespace Tsa.Submissions.Coding.WebApi.Entities;
+
+public static partial class EntityExtensions
+{
+    public static IEnumerable<TestCaseInputResponse> ToResponse(this IEnumerable<TestCaseInput> testCaseInputs)
+    {
+        return testCaseInputs.Select(testCaseInput => testCaseInput.ToResponse());
+    }
+
+    public static TestCaseInputResponse ToResponse(this TestCaseInput testCaseInput)
+    {
+        if (string.IsNullOrWhiteSpace(testCaseInput.DataType)) throw new InvalidOperationException("Test case input data type is required.");
+
+        if (string.IsNullOrWhiteSpace(testCaseInput.Value)) throw new InvalidOperationException("Test case input value is required.");
+
+        return new TestCaseInputResponse(
+            testCaseInput.DataType,
+            testCaseInput.IsArray,
+            testCaseInput.Value,
+            testCaseInput.Index);
+    }
+}
