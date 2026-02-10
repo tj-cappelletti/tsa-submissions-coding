a394827af62b134526e00bd0fed6ac597444abc9
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-02-10 07:38:11 -0500

    Added TestCase enttity
    No longer using MongoDbRef
    Sotoring IDs in document for query performance
    Added initial seed data for problem test cases

---
+++ api/Tsa.Submissions.Coding.WebApi/Entities/TestCaseInput.cs
@@ -0,0 +1,12 @@
+﻿namespace Tsa.Submissions.Coding.WebApi.Entities;
+
+public class TestCaseInput
+{
+    public string? DataType { get; init; }
+
+    public int Index { get; init; }
+
+    public bool IsArray { get; init; }
+
+    public string? Value { get; init; }
+}
---
+++ docker/mongodb/seedData/problem_test_cases.js
@@ -0,0 +1,82 @@
+db = db.getSiblingDB('tsa_submissions_coding');
+
+db.problem_test_cases.createIndex({ problemId: 1, name: 1 }, { unique: true });
+
+db.problem_test_cases.insertMany([
+    {
+        "_id": ObjectId("000000000000000000000001"),
+        "expectedOutput": "0",
+        "inputs": [
+            {
+                "dataType": "String",
+                "isArray": false,
+                "index": 0,
+                "value": "(()"
+            }
+        ],
+        "isActive": true,
+        "isPublic": true,
+        "name": "Test Case 1",
+        "outputDataType": "Int32",
+        "outputIsArray": false,
+        "problemId": ObjectId("000000000000000000000001"),
+        "signature": ""
+    },
+    {
+        "_id": ObjectId("000000000000000000000002"),
+        "expectedOutput": "0",
+        "inputs": [
+            {
+                "dataType": "String",
+                "isArray": false,
+                "index": 0,
+                "value": "))"
+            }
+        ],
+        "isActive": true,
+        "isPublic": true,
+        "name": "Test Case 2",
+        "outputDataType": "Int32",
+        "outputIsArray": false,
+        "problemId": ObjectId("000000000000000000000001"),
+        "signature": ""
+    },
+    {
+        "_id": ObjectId("000000000000000000000003"),
+        "expectedOutput": "0",
+        "inputs": [
+            {
+                "dataType": "String",
+                "isArray": false,
+                "index": 0,
+                "value": "()"
+            }
+        ],
+        "isActive": true,
+        "isPublic": true,
+        "name": "Test Case 3",
+        "outputDataType": "Int32",
+        "outputIsArray": false,
+        "problemId": ObjectId("000000000000000000000001"),
+        "signature": ""
+    },
+    {
+        "_id": ObjectId("000000000000000000000004"),
+        "expectedOutput": "1",
+        "inputs": [
+            {
+                "dataType": "String",
+                "isArray": false,
+                "index": 0,
+                "value": "((()"
+            }
+        ],
+        "isActive": true,
+        "isPublic": true,
+        "name": "Test Case 4",
+        "outputDataType": "Int32",
+        "outputIsArray": false,
+        "problemId": ObjectId("000000000000000000000001"),
+        "signature": ""
+    }
+]);
