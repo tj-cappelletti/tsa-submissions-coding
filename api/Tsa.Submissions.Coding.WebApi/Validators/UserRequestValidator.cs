bb37a1ba3f5036cc165da94a6596cd50c37b4283
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-02-11 11:21:35 -0500

    Refactore UserRequestValidator
    Improve role validation and ensure consistent usage of SubmissionRoles
    Added rule for participants

--- api/Tsa.Submissions.Coding.WebApi/Validators/UserRequestValidator.cs
+++ api/Tsa.Submissions.Coding.WebApi/Validators/UserRequestValidator.cs
@@ -1,21 +1,34 @@
-﻿using FluentValidation;
+﻿using System;
+using FluentValidation;
 using Tsa.Submissions.Coding.Contracts.Users;
+using Tsa.Submissions.Coding.WebApi.Authorization;
 
-namespace Tsa.Submissions.Coding.Contracts.Validators;
+namespace Tsa.Submissions.Coding.WebApi.Validators;
 
 public class UserRequestValidator<T> : AbstractValidator<T> where T : IUserRequest
 {
     public UserRequestValidator()
     {
+        RuleFor(user => user.Participants)
+            .NotEmpty()
+            .When(user => string.Equals(user.Role, SubmissionRoles.Participant, StringComparison.InvariantCultureIgnoreCase))
+            .WithMessage("A participant user must have at least one participant associated with them.");
+
+        RuleFor(user => user.Participants)
+            .NotEmpty()
+            .Must(participants => participants!.Count <= 2)
+            .When(user => string.Equals(user.Role, SubmissionRoles.Participant, StringComparison.InvariantCultureIgnoreCase))
+            .WithMessage("A participant user can have at most two participants associated with them.");
+
         RuleFor(user => user.Role)
             .NotEmpty()
-            .Must(role => string.Equals(role, "Judge", StringComparison.CurrentCultureIgnoreCase) ||
-                          string.Equals(role, "Participant", StringComparison.CurrentCultureIgnoreCase))
+            .Must(role => string.Equals(role, SubmissionRoles.Judge, StringComparison.CurrentCultureIgnoreCase) ||
+                          string.Equals(role, SubmissionRoles.Participant, StringComparison.CurrentCultureIgnoreCase))
             .WithMessage("A user must have a valid role of 'Judge' or 'Participant'.");
 
         RuleFor(user => user.Team)
             .NotNull()
-            .When(user => string.Equals(user.Role, "Participant", StringComparison.InvariantCultureIgnoreCase))
+            .When(user => string.Equals(user.Role, SubmissionRoles.Participant, StringComparison.InvariantCultureIgnoreCase))
             .WithMessage("A participant must be associated with a team.");
 
         RuleFor(user => user.UserName)
