a36bb55d5d44c1faef5a07b15adbdf32bb3f2685
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-02-11 11:31:10 -0500

    Added EntityExtensions for Participant response conversion

---
+++ api/Tsa.Submissions.Coding.WebApi/Entities/EntityExtensions.Participant.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tsa.Submissions.Coding.Contracts.Users;
+
+namespace Tsa.Submissions.Coding.WebApi.Entities;
+
+public static partial class EntityExtensions
+{
+    public static ParticipantResponse ToResponse(this Participant participant)
+    {
+        if (string.IsNullOrWhiteSpace(participant.ParticipantNumber)) throw new InvalidOperationException("Participant number cannot be null or empty.");
+        if (string.IsNullOrWhiteSpace(participant.SchoolNumber))
+            throw new InvalidOperationException("Participant School Number cannot be null or empty.");
+
+        return new ParticipantResponse(
+            participant.ParticipantNumber,
+            participant.SchoolNumber);
+    }
+
+    public static IEnumerable<ParticipantResponse> ToResponses(this IEnumerable<Participant> participants)
+    {
+        return participants.Select(participant => participant.ToResponse());
+    }
+}
