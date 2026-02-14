7b76c9d06b1272fbb79d0e8cb3343622d2af6820
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-02-14 11:44:27 -0500

    Refactored ProblemRequest
    Updated to use properties
    Added JSON property names for proper serilization

--- shared-components/Tsa.Submissions.Coding.Contracts/Problems/ProblemRequest.cs
+++ shared-components/Tsa.Submissions.Coding.Contracts/Problems/ProblemRequest.cs
@@ -1,6 +1,25 @@
-﻿namespace Tsa.Submissions.Coding.Contracts.Problems;
+﻿using System.Text.Json.Serialization;
 
-public record ProblemRequest(
-    string Description,
-    bool IsActive,
-    string Title);
+namespace Tsa.Submissions.Coding.Contracts.Problems;
+
+public record ProblemRequest
+{
+    [JsonPropertyName("description")]
+    public string Description { get; init; }
+
+    [JsonPropertyName("isActive")]
+    public bool IsActive { get; init; }
+
+    [JsonPropertyName("title")]
+    public string Title { get; init; }
+
+    public ProblemRequest(
+        string title,
+        string description,
+        bool isActive)
+    {
+        Description = description;
+        IsActive = isActive;
+        Title = title;
+    }
+}
