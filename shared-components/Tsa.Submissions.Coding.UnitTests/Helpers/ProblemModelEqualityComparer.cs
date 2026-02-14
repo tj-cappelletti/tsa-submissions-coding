0ab01f62d0b28d6161bb176e18d0808af65ec2d4
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-02-14 08:55:33 -0500

    Refactored ProblemModelEqualityComparer
    Updated to use new EqualityComparerBase
    Updated to align with model changes

--- shared-components/Tsa.Submissions.Coding.UnitTests/Helpers/ProblemModelEqualityComparer.cs
+++ shared-components/Tsa.Submissions.Coding.UnitTests/Helpers/ProblemModelEqualityComparer.cs
@@ -3,40 +3,34 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using Tsa.Submissions.Coding.Contracts.Problems;
+using Tsa.Submissions.Coding.UnitTests.Helpers.TestCases;
 
 namespace Tsa.Submissions.Coding.UnitTests.Helpers;
 
 //TODO: Turn into code generator
 [ExcludeFromCodeCoverage]
-internal class ProblemModelEqualityComparer : IEqualityComparer<ProblemResponse?>, IEqualityComparer<IList<ProblemResponse>?>
+internal class ProblemModelEqualityComparer : EqualityComparerBase<ProblemResponse>
 {
-    private readonly TestCaseEqualityComparer _testSetModelEqualityComparer = new();
-
-    public bool Equals(ProblemResponse? x, ProblemResponse? y)
+    protected override bool EqualsCore(ProblemResponse x, ProblemResponse y)
     {
-        if (ReferenceEquals(x, y)) return true;
-        if (x is null) return false;
-        if (y is null) return false;
-        if (x.GetType() != y.GetType()) return false;
+        var descriptionsMatch = x.Description == y.Description;
+        var idsMatch = x.Id == y.Id;
+        var isActiveMatch = x.IsActive == y.IsActive;
+        var testCasesMatch = new TestCaseResponseEqualityComparer().Equals(x.TestCases, y.TestCases);
+        var titlesMatch = x.Title == y.Title;
 
-        return
-            x.Description == y.Description &&
-            x.Id == y.Id &&
-            x.IsActive == y.IsActive &&
-            _testSetModelEqualityComparer.Equals(x.TestCases, y.TestCases) &&
-            x.Title == y.Title;
+        return descriptionsMatch &&
+               idsMatch &&
+               isActiveMatch &&
+               testCasesMatch &&
+               titlesMatch;
     }
 
-    public bool Equals(IList<ProblemResponse>? x, IList<ProblemResponse>? y)
+    protected override bool EqualsCore(IList<ProblemResponse> x, IList<ProblemResponse> y)
     {
-        if (ReferenceEquals(x, y)) return true;
-        if (x is null) return false;
-        if (y is null) return false;
-        if (x.Count != y.Count) return false;
-
         foreach (var leftProblemResponse in x)
         {
-            var rightProblemResponse = y.SingleOrDefault(_ => _.Id == leftProblemResponse.Id);
+            var rightProblemResponse = y.SingleOrDefault(problemResponse => problemResponse.Id == leftProblemResponse.Id);
 
             if (!Equals(leftProblemResponse, rightProblemResponse)) return false;
         }
@@ -44,13 +38,22 @@ internal class ProblemModelEqualityComparer : IEqualityComparer<ProblemResponse?
         return true;
     }
 
-    public int GetHashCode(ProblemResponse obj)
+    public override int GetHashCode(ProblemResponse? obj)
     {
-        return HashCode.Combine(obj.Description, obj.Id, obj.IsActive, obj.TestCases, obj.Title);
+        return obj == null ? 0 : HashCode.Combine(obj.Description, obj.Id, obj.IsActive, obj.TestCases, obj.Title);
     }
 
-    public int GetHashCode(IList<ProblemResponse>? obj)
+    public override int GetHashCode(IList<ProblemResponse>? obj)
     {
-        return obj == null ? 0 : obj.GetHashCode();
+        if (obj == null) return 0;
+
+        var hash = new HashCode();
+
+        foreach (var item in obj.OrderBy(i => i.Id))
+        {
+            hash.Add(GetHashCode(item));
+        }
+
+        return hash.ToHashCode();
     }
 }
