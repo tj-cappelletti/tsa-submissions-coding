ebe6f0e8e1d3025df294dea05073bb4ca37c7270
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-02-15 08:15:31 -0500

    Added JSON property names to to model

--- shared-components/Tsa.Submissions.Coding.Contracts/Languages/ProgrammingLanguageResponse.cs
+++ shared-components/Tsa.Submissions.Coding.Contracts/Languages/ProgrammingLanguageResponse.cs
@@ -1,17 +1,25 @@
-﻿namespace Tsa.Submissions.Coding.Contracts.Languages;
+﻿using System.Text.Json.Serialization;
+
+namespace Tsa.Submissions.Coding.Contracts.Languages;
 
 public record ProgrammingLanguageResponse
 {
+    [JsonPropertyName("fileExtension")]
     public string FileExtension { get; set; }
 
+    [JsonPropertyName("id")]
     public string Id { get; set; }
 
+    [JsonPropertyName("identifier")]
     public string Identifier { get; set; }
 
+    [JsonPropertyName("isEnabled")]
     public bool IsEnabled { get; set; } = true;
 
+    [JsonPropertyName("name")]
     public string Name { get; set; } = string.Empty;
 
+    [JsonPropertyName("versions")]
     public List<ProgrammingLanguageVersionResponse> Versions { get; set; } = [];
 
     public ProgrammingLanguageResponse(
