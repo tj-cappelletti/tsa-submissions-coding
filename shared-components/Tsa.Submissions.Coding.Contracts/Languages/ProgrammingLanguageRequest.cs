aa1e26dd38f8fced941a822b1d422f38879e34af
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-02-17 11:17:41 -0500

    Created request models for ProgrammingLanguage and ProgrammingLanguageVersion

---
+++ shared-components/Tsa.Submissions.Coding.Contracts/Languages/ProgrammingLanguageRequest.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Tsa.Submissions.Coding.Contracts.Languages;
+
+public record ProgrammingLanguageRequest
+{
+    [JsonPropertyName("fileExtension")]
+    public string FileExtension { get; set; }
+
+    [JsonPropertyName("identifier")]
+    public string Identifier { get; set; }
+
+    [JsonPropertyName("isEnabled")]
+    public bool IsEnabled { get; set; } = true;
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; } = string.Empty;
+
+    public ProgrammingLanguageRequest(
+        string identifier,
+        string name,
+        string fileExtension,
+        bool isEnabled)
+    {
+        FileExtension = fileExtension;
+        Identifier = identifier;
+        IsEnabled = isEnabled;
+        Name = name;
+    }
+}
