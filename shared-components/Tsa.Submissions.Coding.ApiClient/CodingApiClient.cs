250da387552ab4bb4c10c7a3d9663fbb0d14f6a5
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-02-24 08:21:32 -0500

    Added param checking to CodingApiClient constructor

--- shared-components/Tsa.Submissions.Coding.ApiClient/CodingApiClient.cs
+++ shared-components/Tsa.Submissions.Coding.ApiClient/CodingApiClient.cs
@@ -18,6 +18,19 @@ public class CodingApiClient : ICodingApiClient
 
     public CodingApiClient(Uri baseUri, string username, string password, string loginEndpoint = "api/auth/login")
     {
+        if(baseUri == null) throw new ArgumentNullException(nameof(baseUri));
+        
+        if(username == null) throw new ArgumentNullException(nameof(username));
+        if(string.IsNullOrWhiteSpace(username))
+            throw new ArgumentException("Username cannot be null, empty, or whitespace.", nameof(username));
+
+        if(password == null) throw new ArgumentNullException(nameof(password));
+        if(string.IsNullOrWhiteSpace(password))
+            throw new ArgumentException("Password cannot be null, empty, or whitespace.", nameof(password));
+
+        if (string.IsNullOrWhiteSpace(loginEndpoint))
+            throw new ArgumentException("Login endpoint cannot be null, empty, or whitespace.", nameof(loginEndpoint));
+
         _loginEndpoint = loginEndpoint;
         _password = password;
         _username = username;
