9bb670f9bd6a22b413b7ac2d2dedba1e95c8f6f4
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-02-24 08:22:23 -0500

    Established mock builder pattern for services
    This reduces copy/paste code
    Improves redability for unit tests

---
+++ shared-components/Tsa.Submissions.Coding.UnitTests/Helpers/Mocks/MockedProgrammingLanguagesServiceBuilder.cs
@@ -0,0 +1,8 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Tsa.Submissions.Coding.WebApi.Entities;
+using Tsa.Submissions.Coding.WebApi.Services;
+
+namespace Tsa.Submissions.Coding.UnitTests.Helpers.Mocks;
+
+[ExcludeFromCodeCoverage]
+internal class MockedProgrammingLanguagesServiceBuilder : MockedServiceBuilder<IProgrammingLanguagesService, ProgrammingLanguage>;
