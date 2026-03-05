9921740af991f0d7a458b010cd078812736bdd5e
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-03-05 09:14:17 -0500

    Add SubmissionListResponse record for listing submission details

---
+++ shared-components/Tsa.Submissions.Coding.Contracts/Submissions/SubmissionListResponse.cs
@@ -0,0 +1,40 @@
+﻿using Tsa.Submissions.Coding.Contracts.Users;
+
+namespace Tsa.Submissions.Coding.Contracts.Submissions;
+
+public record SubmissionListResponse
+{
+    public string Id { get; init; }
+
+    public string ProblemId { get; init; }
+
+    public string ProgrammingLanguageId { get; init; }
+
+    public string ProgrammingLanguageVersionTag { get; init; }
+
+    // TODO: Need to come up with a way to summarize the scorecard for a list response
+    // public SubmissionScorecard? Scorecard { get; set; }
+
+    public DateTimeOffset SubmittedOn { get; init; }
+
+    // TODO: Need to come up with a way to summarize test case results for a list response
+    // public List<TestCaseResult> TestCaseResults { get; init; } = [];
+
+    public UserResponse User { get; init; }
+
+    public SubmissionListResponse(
+        string id,
+        string problemId,
+        string programmingLanguageId,
+        string programmingLanguageVersionTag,
+        DateTimeOffset submittedOn,
+        UserResponse user)
+    {
+        Id = id;
+        ProblemId = problemId;
+        ProgrammingLanguageId = programmingLanguageId;
+        ProgrammingLanguageVersionTag = programmingLanguageVersionTag;
+        SubmittedOn = submittedOn;
+        User = user;
+    }
+}
