fb1fc10dbadf411bb7e33873e12914e787060196
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-03-12 14:40:23 -0400

    Add MockedUsersServiceBuilder for IUsersService unit testing

---
+++ shared-components/Tsa.Submissions.Coding.UnitTests/Helpers/Users/MockedUsersServiceBuilder.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Moq;
+using Tsa.Submissions.Coding.UnitTests.Helpers.Mocks;
+using Tsa.Submissions.Coding.WebApi.Entities;
+using Tsa.Submissions.Coding.WebApi.Services;
+
+namespace Tsa.Submissions.Coding.UnitTests.Helpers.Users;
+
+[ExcludeFromCodeCoverage]
+internal class MockedUsersServiceBuilder : MockedServiceBuilder<IUsersService, User>
+{
+    public override MockedServiceBuilder<IUsersService, User> WithCreateAsync(User expectedEntity, string newId, Times? times = null)
+    {
+        WithCreateAsync(expectedEntity, newId, new UserEqualityComparer(), times);
+
+        return this;
+    }
+
+    public override MockedServiceBuilder<IUsersService, User> WithRemoveAsync(User expectedEntity, Times? times = null)
+    {
+        WithRemoveAsync(expectedEntity, new UserEqualityComparer(), times);
+
+        return this;
+    }
+
+    public override MockedServiceBuilder<IUsersService, User> WithUpdateAsync(User expectedEntity, Times? times = null)
+    {
+        WithUpdateAsync(expectedEntity, new UserEqualityComparer(), times);
+
+        return this;
+    }
+}
