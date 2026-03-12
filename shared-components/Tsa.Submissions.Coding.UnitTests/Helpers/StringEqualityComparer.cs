756f643476d1b1295aea16a4ad802c44b03e5299
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-03-12 10:59:12 -0400

    Refactored StringEqualityComparer to use EqualityComparerBase

--- shared-components/Tsa.Submissions.Coding.UnitTests/Helpers/StringEqualityComparer.cs
+++ shared-components/Tsa.Submissions.Coding.UnitTests/Helpers/StringEqualityComparer.cs
@@ -1,19 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 
 namespace Tsa.Submissions.Coding.UnitTests.Helpers;
 
 [ExcludeFromCodeCoverage]
-internal class StringEqualityComparer : IEqualityComparer<string?>
+internal class StringEqualityComparer : EqualityComparerBase<string>
 {
-    public bool Equals(string? x, string? y)
+    protected override bool EqualsCore(string x, string y)
     {
-        return string.Equals(x, y, StringComparison.InvariantCultureIgnoreCase);
+        return x == y;
     }
 
-    public int GetHashCode(string? obj)
+    public override int GetHashCode(string? obj)
     {
-        return obj == null ? 0 : obj.GetHashCode(StringComparison.InvariantCultureIgnoreCase);
+        return obj == null
+            ? 0
+            : obj.GetHashCode(StringComparison.CurrentCulture);
+    }
+
+    protected override Func<string, bool> GetItemPredicate(string item)
+    {
+        return @string => @string == item;
+    }
+
+    protected override object GetOrderByKey(string item)
+    {
+        return item;
     }
 }
