b0a50995cdd4de885b8ba744ae6710e44f50bb3c
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-03-12 14:39:02 -0400

    Add SubmissionCreateRequestValidator for submission validation

---
+++ api/Tsa.Submissions.Coding.WebApi/Validators/SubmissionCreateRequestValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using Tsa.Submissions.Coding.Contracts.Submissions;
+
+namespace Tsa.Submissions.Coding.WebApi.Validators;
+
+public class SubmissionCreateRequestValidator : AbstractValidator<SubmissionCreateRequest>
+{
+    public SubmissionCreateRequestValidator()
+    {
+        RuleFor(submission => submission.ProblemId)
+            .NotEmpty()
+            .WithMessage("The Problem ID is required for a submission.");
+
+        RuleFor(submission => submission.ProgrammingLanguageId)
+            .NotEmpty()
+            .WithMessage("The Programming Language ID is required for a submission.");
+
+        RuleFor(submission => submission.ProgrammingLanguageVersionTag)
+            .NotEmpty()
+            .WithMessage("The Programming Language Version Tag is required for a submission.");
+
+        RuleFor(submission => submission.Solution)
+            .NotEmpty()
+            .WithMessage("A solution is required for a submission.");
+    }
+}
