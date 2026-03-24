a4444be573e3d18fe951590be37f7d9e70985a98
TJ Cappelletti <20914729+tj-cappelletti@users.noreply.github.com>
2026-03-24 06:25:31 -0400

    Created ToResponse for TestCaseResult

---
+++ api/Tsa.Submissions.Coding.WebApi/Entities/EntityExtensions.TestCaseResult.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Tsa.Submissions.Coding.Contracts.TestCases;
+
+namespace Tsa.Submissions.Coding.WebApi.Entities;
+
+public partial class EntityExtensions
+{
+    public static TestCaseResultResponse ToResponse(this TestCaseResult testCaseResult)
+    {
+        if (string.IsNullOrWhiteSpace(testCaseResult.TestCaseId))
+        {
+            throw new InvalidOperationException("The Test Case ID is required for a Test Case Result");
+        }
+
+        return new TestCaseResultResponse(
+            testCaseResult.TestCaseId,
+            testCaseResult.ActualOutput,
+            testCaseResult.Message,
+            testCaseResult.Passed,
+            testCaseResult.TimedOut,
+            testCaseResult.ExecutionTime);
+    }
+}
