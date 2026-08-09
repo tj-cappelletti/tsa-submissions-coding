edcbb4d94f7dd02858cef8f19d09e5346ac82ee1
TJ Cappelletti <tcappelletti@microsoft.com>
2026-08-09 10:41:42 -0400

    Added UserRoles class for authorization management

---
+++ src/Domain/Authorization/UserRoles.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tsa.Submissions.Coding.Domain.Authorization
+{
+    public static class UserRoles
+    {
+        public const string Judge = "Judge";
+        public const string Participant = "Participant";
+    }
+}
