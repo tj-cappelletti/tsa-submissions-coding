57659e51613a9d617276f1c565e26f275a811cb3
TJ Cappelletti <tcappelletti@microsoft.com>
2026-08-09 10:50:17 -0400

    Added CodingEvent entity and CompetitionLevel enum for event management

---
+++ src/Domain/Entities/CodingEvent.cs
@@ -0,0 +1,43 @@
+﻿using Tsa.Submissions.Coding.Domain.Common.Auditing;
+
+namespace Tsa.Submissions.Coding.Domain.Entities
+{
+    public class CodingEvent : AuditableEntity
+    {
+        public CompetitionLevel CompetitionLevel { get; private set; }
+
+        public string Name { get; private set; } = string.Empty;
+        
+        public string Description { get; private set; } = string.Empty;
+        
+        public DateTime StartDate { get; private set; }
+        
+        public DateTime EndDate { get; private set; }
+
+        private CodingEvent() { } // for ORM
+
+        public static CodingEvent Create(CompetitionLevel competitionLevel, string name, string description, DateTime startDate, DateTime endDate, User createdBy)
+        {
+            if (string.IsNullOrWhiteSpace(name)) throw new ArgumentException("Name is required.", nameof(name));
+
+            if (string.IsNullOrWhiteSpace(description)) throw new ArgumentException("Description is required.", nameof(description));
+
+            if (startDate >= endDate) throw new ArgumentException("Start date must be before end date.", nameof(startDate));
+
+            ArgumentNullException.ThrowIfNull(createdBy);
+
+            var codingEvent = new CodingEvent
+            {
+                CompetitionLevel = competitionLevel,
+                EndDate = endDate,
+                Description = description.Trim(),
+                Name = name.Trim(),
+                StartDate = startDate,
+            };
+            
+            codingEvent.SetCreatedAudit(createdBy.UserName, createdBy.Id);
+
+            return codingEvent;
+        }
+    }
+}
---
+++ src/Domain/Entities/CompetitionLevel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tsa.Submissions.Coding.Domain.Entities;
+
+public enum CompetitionLevel
+{
+    MiddleSchool = 1,
+    HighSchool = 2
+}
