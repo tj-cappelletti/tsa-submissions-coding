592b146cde36af632684d691de96db3f188c0ecb
TJ Cappelletti <tcappelletti@microsoft.com>
2026-08-09 10:42:39 -0400

    Add AuditableEntity class and IAuditableEntity interface for auditing functionality

---
+++ src/Domain/Common/Auditing/AuditableEntity.cs
@@ -0,0 +1,35 @@
+﻿namespace Tsa.Submissions.Coding.Domain.Common.Auditing
+{
+    public abstract class AuditableEntity : Entity, IAuditableEntity
+    {
+        public DateTime CreatedAt { get; protected set; }
+
+        public string CreatedBy { get; protected set; } = string.Empty;
+
+        public Guid CreatedById { get; protected set; }
+
+        public DateTime UpdatedAt { get; protected set; }
+
+        public string UpdatedBy { get; protected set; } = string.Empty;
+
+        public Guid UpdatedById { get; protected set; }
+
+        protected void SetCreatedAudit(string userIdentifier, Guid userId)
+        {
+            CreatedAt = DateTime.UtcNow;
+            CreatedBy = userIdentifier;
+            CreatedById = userId;
+
+            UpdatedAt = CreatedAt;
+            UpdatedBy = userIdentifier;
+            UpdatedById = userId;
+        }
+
+        protected void SetUpdatedAudit(string userIdentifier, Guid userId)
+        {
+            UpdatedAt = DateTime.UtcNow;
+            UpdatedBy = userIdentifier;
+            UpdatedById = userId;
+        }
+    }
+}
---
+++ src/Domain/Common/Auditing/IAuditableEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace Tsa.Submissions.Coding.Domain.Common.Auditing;
+
+public interface IAuditableEntity
+{
+    DateTime CreatedAt { get; }
+
+    string CreatedBy { get; }
+
+    Guid CreatedById { get; }
+
+    DateTime UpdatedAt { get; }
+
+    string UpdatedBy { get; }
+
+    Guid UpdatedById { get; }
+}
